b059622fa46f6b690d83753ca6567567156aee01
Nokitoo <guigui4700@gmail.com>
2016-04-08 18:17:59 +0200

    delivery finish

--- www/js/controllers/Deliveries/Modals/FinishCtrl.js
+++ www/js/controllers/Deliveries/Modals/FinishCtrl.js
@@ -1,6 +1,7 @@
 angular.module('shopmycourse.controllers')
 
-.controller('DeliveriesFinishCtrl', function($scope, $ionicSlideBoxDelegate) {
+.controller('DeliveriesFinishCtrl', function($scope, $ionicSlideBoxDelegate, DeliveryAPI) {
+	$scope.ratingStar = 0;
 
   $scope.disableSwipe = function() {
     $ionicSlideBoxDelegate.enableSlide(false);
@@ -8,5 +9,17 @@ angular.module('shopmycourse.controllers')
 
   $scope.nextSlide = function () {
     $ionicSlideBoxDelegate.next();
+  }
+
+  $scope.setRatingStar = function(newRating) {
+	$scope.ratingStar = newRating;
+  }
+
+  $scope.finalizeDelivery = function(delivery, validation_code) {
+	DeliveryAPI.finalize({'idDelivery': delivery.id, 'validation_code': validation_code, 'rating': $scope.ratingStar}, function() {
+		$scope.nextSlide();
+	}, function (err) {
+		console.error(err);
+	});
   }
 })
