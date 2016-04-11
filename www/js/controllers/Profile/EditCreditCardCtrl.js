6a752af00c6c7e27b297b88f55a9e740405e39b1
Nokitoo <guigui4700@gmail.com>
2016-04-11 15:11:26 +0200

    EditCreditCart template + ctrl

--- www/js/controllers/Profile/EditCreditCardCtrl.js
+++ www/js/controllers/Profile/EditCreditCardCtrl.js
@@ -1,12 +1,46 @@
 angular.module('shopmycourse.controllers')
 
-.controller('ProfileEditCreditCardCtrl', function($scope, $state, $ionicHistory, $ionicViewSwitcher) {
-  $scope.endEdit = function () {
-    $ionicViewSwitcher.nextDirection('back');
-    $ionicHistory.nextViewOptions({
-      disableAnimate: false,
-      disableBack: true
+.controller('ProfileEditCreditCardCtrl', function($scope, $state, $ionicHistory, $ionicViewSwitcher, Validation, CardAPI, CurrentUser) {
+    $scope.validation = Validation;
+    $scope.card = {};
+    $scope.expirations = []
+    $scope.user = {};
+
+    CurrentUser.get(function (user) {
+        $scope.user = user;
+    })
+
+    var today = moment();
+
+    //iterate for i years of expiration
+    for (i = 0; i < 10; i++) {
+        var date = {
+            'year': today.year(),
+            'months': []
+        }
+
+
+        for (j = today.month(); j <= 12; j++) {
+            date.months.push(j < 10 ? "0" + j : j);
+        }
+
+        $scope.expirations.push(date);
+
+        today.month(1);
+        today.add(1, 'years');
+    }
+
+  $scope.endEdit = function ($event) {
+    $event.preventDefault();
+    var card = {
+        number: $scope.card.number,
+        type: 0,
+        date: $scope.card.month + '/' + $scope.card.year.year,
+        cvv: $scope.card.cvv
+    }
+
+    CardAPI.update({idUser: $scope.user.id, card: card}, function() {
+        $state.go('tabs.home');
     });
-    $state.go('tabs.profile');
   }
 })
