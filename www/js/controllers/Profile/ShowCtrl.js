4e3f03369bcd86edfe561d3e539416bd56453ace
Caligone <pierrejean.leger.pro@gmail.com>
2016-04-12 07:57:52 +0200

    Popup on share_phone toggle on the show profile page

--- www/js/controllers/Profile/ShowCtrl.js
+++ www/js/controllers/Profile/ShowCtrl.js
@@ -1,6 +1,6 @@
 angular.module('shopmycourse.controllers')
 
-.controller('ProfileShowCtrl', function($scope, $state, Authentication, CurrentUser) {
+.controller('ProfileShowCtrl', function($scope, $state, $ionicPopup, Authentication, CurrentUser, UserAPI) {
 
   $scope.user = {};
   CurrentUser.get(function (user) {
@@ -11,6 +11,29 @@ angular.module('shopmycourse.controllers')
   CurrentUser.getAvatar(function (avatar) {
     $scope.avatar = avatar;
   });
+
+  $scope.togglePhone = function () {
+    if (!$scope.user.share_phone) {
+      var confirmPopup = $ionicPopup.confirm({
+        title: 'Afficher son numéro de téléphone',
+        template: 'Êtes-vous sûr de vouloir afficher votre numéro de téléphone?'
+      });
+
+      confirmPopup.then(function (res) {
+        $scope.user.share_phone = !res;
+        UserAPI.update($scope.user, function (user) {
+          CurrentUser.set(user, function() {});
+          $scope.user = user;
+        });
+      });
+    } else {
+      UserAPI.update($scope.user, function (user) {
+        CurrentUser.set(user, function() {});
+        $scope.user = user;
+      });
+    }
+
+  };
 
 
   $scope.logout = function () {
