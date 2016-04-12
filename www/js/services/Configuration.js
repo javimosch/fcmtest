b9cb06f0c63e76e5886dd412d52278577733dc16
Caligone <pierrejean.leger.pro@gmail.com>
2016-04-12 10:59:53 +0200

    Add errors slug from server

--- www/js/services/Configuration.js
+++ www/js/services/Configuration.js
@@ -6,7 +6,12 @@ angular.module('shopmycourse.services')
     apiEndpoint: 'http://127.0.0.1:3000/',
     errors: {
       SCHEDULE_ALREADY_EXIST: 'Vous avez déjà déposé une disponibilité',
-      VALIDATION_CODE_ERROR: 'Votre code de validation est incorrect'
+      VALIDATION_CODE_ERROR: 'Votre code de validation est incorrect',
+      ORDER_DONE: 'La commande a été effectuée',
+      LEMONWAY_SERVER_ERROR: 'Une erreur liée à votre paiement est survenue',
+      WALLET_ERROR: 'Une erreur liée à votre paiement est survenue',
+      RATING_DONE: 'Votre avis a été pris en compte',
+      VALIDATION_CODE_ERROR: 'Une erreur est survenue lors de la validation'
     }
   };
 });
