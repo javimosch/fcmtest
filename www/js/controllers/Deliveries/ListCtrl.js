14a4147d13043504f2da1dfeb11663ec2135460b
Caligone <pierrejean.leger.pro@gmail.com>
2016-04-12 14:03:17 +0200

    Add loader on Deliveries list page

--- www/js/controllers/Deliveries/ListCtrl.js
+++ www/js/controllers/Deliveries/ListCtrl.js
@@ -1,14 +1,20 @@
 angular.module('shopmycourse.controllers')
 
-.controller('DeliveriesListCtrl', function($scope, $state, DeliveryStore) {
+.controller('DeliveriesListCtrl', function($scope, $ionicLoading, $state, DeliveryStore) {
 
   $scope.deliveries = [];
   $scope.status = 'pending';
 
+  $ionicLoading.show({
+    template: 'Nous recherchons vos livraisons...'
+  });
+
   DeliveryStore.get({}, function (err, deliveries) {
     $scope.deliveries = deliveries;
+    $ionicLoading.hide();
   }, function (err) {
     console.error(err);
+    $ionicLoading.hide();
   });
 
   $scope.byStatus = function (status) {
