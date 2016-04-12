8ca30d47d06d03b7d8a283f41f2b569e95947dd7
Caligone <pierrejean.leger.pro@gmail.com>
2016-04-12 08:07:11 +0200

    Popup on share_phone toggle on the edit profile page

--- www/js/controllers/Profile/EditCtrl.js
+++ www/js/controllers/Profile/EditCtrl.js
@@ -1,6 +1,6 @@
 angular.module('shopmycourse.controllers')
 
-.controller('ProfileEditCtrl', function($scope, $state, $ionicHistory, $ionicViewSwitcher, Validation, CurrentUser, UserAPI) {
+.controller('ProfileEditCtrl', function($scope, $state, $ionicHistory, $ionicViewSwitcher, $ionicPopup, Validation, CurrentUser, UserAPI) {
 
   $scope.validation = Validation;
 
@@ -13,6 +13,31 @@ angular.module('shopmycourse.controllers')
   CurrentUser.getAvatar(function (avatar) {
     $scope.avatar = avatar;
   });
+
+  $scope.togglePhone = function () {
+    CurrentUser.get(function (user) {
+      user.share_phone = $scope.user.share_phone;
+      if (!user.share_phone) {
+        var confirmPopup = $ionicPopup.confirm({
+          title: 'Afficher son numéro de téléphone',
+          template: 'Êtes-vous sûr de vouloir afficher votre numéro de téléphone?'
+        });
+
+        confirmPopup.then(function (res) {
+          user.share_phone = !res;
+          UserAPI.update(user, function (user) {
+            CurrentUser.set(user, function() {});
+            $scope.user.share_phone = user.share_phone;
+          });
+        });
+      } else {
+        UserAPI.update(user, function (user) {
+          CurrentUser.set(user, function() {});
+          $scope.user.share_phone = user.share_phone;
+        });
+      }
+    });
+  };
 
   $scope.endEdit = function () {
     UserAPI.update($scope.user, function (correct, errorCode) {
