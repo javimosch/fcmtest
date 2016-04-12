a6a3344f9d49c14de21f624f106c5b205fbc207d
Caligone <pierrejean.leger.pro@gmail.com>
2016-04-12 08:37:55 +0200

    Fix an issue about phone sharing on the edit profile page

--- www/js/controllers/Profile/EditCtrl.js
+++ www/js/controllers/Profile/EditCtrl.js
@@ -17,14 +17,14 @@ angular.module('shopmycourse.controllers')
   $scope.togglePhone = function () {
     CurrentUser.get(function (user) {
       user.share_phone = $scope.user.share_phone;
-      if (!user.share_phone) {
+      if (user.share_phone) {
         var confirmPopup = $ionicPopup.confirm({
           title: 'Afficher son numéro de téléphone',
           template: 'Êtes-vous sûr de vouloir afficher votre numéro de téléphone?'
         });
 
         confirmPopup.then(function (res) {
-          user.share_phone = !res;
+          user.share_phone = res;
           UserAPI.update(user, function (user) {
             CurrentUser.set(user, function() {});
             $scope.user.share_phone = user.share_phone;
