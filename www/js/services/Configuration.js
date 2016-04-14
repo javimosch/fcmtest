5cd64d04cc621403faf78dedbe30cfa3459dfc6c
Nokitoo <guigui4700@gmail.com>
2016-04-14 12:06:42 +0200

    add EMPTY_CART error message

--- www/js/services/Configuration.js
+++ www/js/services/Configuration.js
@@ -9,7 +9,8 @@ angular.module('shopmycourse.services')
       VALIDATION_CODE_ERROR: 'Votre code de validation est incorrect',
       LEMONWAY_SERVER_ERROR: 'Une erreur liée à votre paiement est survenue',
       WALLET_ERROR: 'Une erreur liée à votre paiement est survenue',
-      VALIDATION_CODE_ERROR: 'Une erreur est survenue lors de la validation'
+      VALIDATION_CODE_ERROR: 'Une erreur est survenue lors de la validation',
+      EMPTY_CART: 'Vous devez remplir votre panier'
     },
     success: {
       ORDER_DONE: 'La commande a été effectuée',
