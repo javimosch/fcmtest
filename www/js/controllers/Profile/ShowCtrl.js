85719926d9771e60c1f18987f6d3698077d7d49d
javoche <arancibiajav@gmail.com>
2016-09-08 13:08:55 +0000

    issue-browser-logout

--- www/js/controllers/Profile/ShowCtrl.js
+++ www/js/controllers/Profile/ShowCtrl.js
@@ -5,30 +5,32 @@ angular.module('shopmycourse.controllers')
  * @function Controleur
  * @memberOf shopmycourse.controllers
  * @description Page des paramètres
-*/
+ */
 
 .controller('ProfileShowCtrl', function($scope, $ionicLoading, $state, $ionicPopup, $ionicModal, Authentication, CurrentUser, CurrentAddress, UserAPI) {
 
   /**
    * Initialisation de la valeur du portefeuille à 0
-  */
+   */
   $scope.walletValue = 0;
 
   /**
    * Affichage du message de chargement pour récupérer le profil
-  */
+   */
   $ionicLoading.show({
     template: 'Nous récupérons votre profil...'
   });
 
   /**
    * Chargement de l'utilisateur actuel
-  */
+   */
   $scope.user = {};
-  CurrentUser.get(function (user) {
+  CurrentUser.get(function(user) {
     $scope.user = user;
     $scope.avatarBackground = CurrentUser.getAvatar();
-    UserAPI.getWallet({idUser: user.id}, function (wallet) {
+    UserAPI.getWallet({
+      idUser: user.id
+    }, function(wallet) {
       $scope.walletValue = wallet.value;
     });
     $ionicLoading.hide();
@@ -37,29 +39,30 @@ angular.module('shopmycourse.controllers')
   /**
    * @name $scope.togglePhone
    * @description Affichage d'une popup de confirmation quand il y a modification des options de partage du téléphone
-  */
-  $scope.togglePhone = function () {
+   */
+  $scope.togglePhone = function() {
     if (!$scope.user.share_phone) {
       var confirmPopup = $ionicPopup.confirm({
         title: 'Masquer son numéro de téléphone',
         template: 'Êtes-vous sûr de vouloir masquer votre numéro de téléphone ?<br>On vous le déconseille afin de pouvoir communiquer plus facilement avec les autres utilisateurs, si un article est manquant par exemple.<br>De plus cela peut détériorer la note laissée par les autres utilisateurs.'
       });
-      confirmPopup.then(function (res) {
+      confirmPopup.then(function(res) {
         $scope.user.share_phone = !res;
         $ionicLoading.show({
           template: 'Nous sauvegardons vos préférences...'
         });
-        UserAPI.update($scope.user, function (user) {
+        UserAPI.update($scope.user, function(user) {
           $ionicLoading.hide();
           CurrentUser.set(user, function() {});
           $scope.user = user;
         });
       });
-    } else {
+    }
+    else {
       $ionicLoading.show({
         template: 'Nous sauvegardons vos préférences...'
       });
-      UserAPI.update($scope.user, function (user) {
+      UserAPI.update($scope.user, function(user) {
         $ionicLoading.hide();
         CurrentUser.set(user, function() {});
         $scope.user = user;
@@ -69,7 +72,7 @@ angular.module('shopmycourse.controllers')
 
   /**
    * Affichage de la popup "Charte de confidentialité"
-  */
+   */
   $ionicModal.fromTemplateUrl('templates/Privacy.html', {
     scope: $scope,
     animation: 'slide-in-up'
@@ -85,29 +88,33 @@ angular.module('shopmycourse.controllers')
 
   /**
    * Affichage de la popup "Conditions générales d'utilisation"
-  */
+   */
   $ionicModal.fromTemplateUrl('templates/CGU.html', {
     scope: $scope,
     animation: 'slide-in-up'
-  }).then(function (modal) {
+  }).then(function(modal) {
     $scope.modalCGU = modal;
   });
-  $scope.openCGU = function () {
+  $scope.openCGU = function() {
     $scope.modalCGU.show();
   };
-  $scope.closeCGU = function () {
+  $scope.closeCGU = function() {
     $scope.modalCGU.hide();
   };
 
   /**
    * @name $scope.logout
    * @description Lancement de la déconnexion
-  */
-  $scope.logout = function () {
+   */
+  $scope.logout = function() {
     Authentication.logout(function() {
-      window.plugins.googleplus.disconnect(function (msg) {
-        console.log(msg);
-      });
+
+      if (window.plugins && window.plugins.googleplus) {
+        window.plugins.googleplus.disconnect(function(msg) {
+          console.log(msg);
+        });
+      }
+
       $state.go('start');
     });
   };
