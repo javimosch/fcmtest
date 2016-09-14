06a450b0e18439ff20d9c374a991af99c72ee785
cchardeau <corentin.chardeau@epitech.eu>
2016-09-14 17:59:43 +0200

    Add notifications check when app come back to foreground

--- www/js/app.js
+++ www/js/app.js
@@ -16,7 +16,7 @@ angular.module('shopmycourse', [
   'shopmycourse.directives'
 ])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $rootScope, $ionicModal, NotificationAPI, CurrentDelivery) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -27,6 +27,47 @@ angular.module('shopmycourse', [
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+    document.addEventListener('resume', function() {
+      $ionicModal.fromTemplateUrl('templates/NotificationsModal.html', {
+          scope: $rootScope,
+          animation: 'slide-in-up'
+      }).then(function (modal) {
+        $rootScope.notificationsModal = modal
+      });
+      $rootScope.openNotificationsModal = function () {
+        $rootScope.notificationsModal.show();
+      };
+      $rootScope.closeNotificationsModal = function () {
+        $rootScope.notificationsModal.hide();
+      };
+      $rootScope.notifications = [];
+      NotificationAPI.get({}, function (notifications) {
+        $rootScope.notifications = window._.map(notifications, function (n) {
+          n.meta = JSON.parse(n.meta);
+          switch (n.mode) {
+            case 'delivery_request':
+              n.meta.buyer.rating_average = n.meta.buyer.rating_average || 0;
+              break;
+            case 'accepted_delivery':
+              CurrentDelivery.clear();
+              n.meta.deliveryman.rating_average = n.meta.deliveryman.rating_average || 0;
+              break;
+            case 'order_reminder':
+              n.meta.deliveryman.rating_average = n.meta.deliveryman.rating_average || 0;
+              break;
+            case 'cart_filled':
+              n.meta.buyer.rating_average = n.meta.buyer.rating_average || 0;
+              break;
+          }
+          return n;
+        });
+        if (notifications.length > 0) {
+          $rootScope.openNotificationsModal();
+        }
+      }, function (err) {
+        console.error('Notifications error : ', err);
+      });
+    }, false);
   });
 })
 
