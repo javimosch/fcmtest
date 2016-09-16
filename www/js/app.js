1885692995c61344568e8a276b6bf1741dc62d91
javoche <arancibiajav@gmail.com>
2016-09-16 08:32:32 +0000

    added-coded-format-app
    just code format (styling)

--- www/js/app.js
+++ www/js/app.js
@@ -20,17 +20,17 @@ angular.module('shopmycourse', [
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
 })
 
-.config(function ($httpProvider) {
+.config(function($httpProvider) {
   $httpProvider.interceptors.push('HTTPInterceptor');
 })
 
@@ -48,27 +48,32 @@ angular.module('shopmycourse', [
 
 
 
-.run(function(DeliveryRequestAPI,Browser,$localForage) {
+.run(function(DeliveryRequestAPI, Browser, $localForage) {
   window.DeliveryRequestAPI = DeliveryRequestAPI;
   window.Browser = Browser
   window.$localForage = $localForage
 })
 
 
-.config(['$localForageProvider', function($localForageProvider){
-    $localForageProvider.config({
-      driver      : 'localStorageWrapper',
-      name        : 'ShopMyCourse',
-      version     : 1.0,
-      storeName   : 'main',
-      description : 'Main local database for ShopMyCourse mobile app'
-    });
+.config(['$localForageProvider', function($localForageProvider) {
+  $localForageProvider.config({
+    driver: 'localStorageWrapper',
+    name: 'ShopMyCourse',
+    version: 1.0,
+    storeName: 'main',
+    description: 'Main local database for ShopMyCourse mobile app'
+  });
 }])
 
-.filter('replaceHttp', function () {
-  return function (url) {
-    if(url) {
+.filter('replaceHttp', function() {
+  return function(url) {
+    if (url) {
       return url.replace(/http:/g, 'https:');
     }
   };
 });
+
+
+
+
+
