1dfc6b7949c97b4680897617287d090572b909d1
javoche <arancibiajav@gmail.com>
2016-09-16 10:12:06 +0000

    added-state-history-service
    stateHistory can be helpful if you want to goBack to the last route with the exact parameters.
    Example: User press the arrow back in the Order List of Products Search List.
    Before there was only one route for Order Details view. Now there are two. To solve this problem we just use $stateHistory.goBack() and the service is in charge of guess what was the last route.

--- www/js/services.js
+++ www/js/services.js
@@ -17,9 +17,37 @@ angular.module('shopmycourse.services', ['ngResource'])
     M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
     if ((tem = ua.match(/version\/(\d+)/i)) != null) M.splice(1, 1, tem[1]);
     return M.join('');
-  })().replace(' ','').toLowerCase();
+  })().replace(' ', '').toLowerCase();
 
   return {
     name: getName
   }
+})
+
+.service('$stateHistory', function($state, $rootScope) {
+  var self = {};
+  var _history = [];
+  self.push = function(name, params) {
+    _history.push({
+      name: name,
+      params: params
+    });
+    //console.log('history has now',_history.length,'states');
+  };
+  self.goBack = function(settings) {
+    if (_history.length <= 1) {
+      if (settings.onFailure) settings.onFailure();
+      else console.log('$stateHistory history length', _history.length);
+      return
+    }
+    _history.pop();
+    var last = _history[_history.length - 1];
+    $state.go(last.name, last.params);
+    if (settings.onSuccess) settings.onSuccess();
+  };
+  $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+    self.push(toState.name, toParams);
+  });
+  return self;
 });
+
